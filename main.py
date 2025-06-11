24a63ccb0da6def2ee438283b8c563b7426c2d25
lymvs <lysandros.mavropoulos@gmail.com>
2025-06-11 14:35:00 +0000

    Add system prompt

--- main.py
+++ main.py
@@ -5,12 +5,15 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
+from prompts import system_prompt
 
 def main():
     load_dotenv()
+
+    verbose = "--verbose" in sys.argv
     api_key = os.environ.get("GEMINI_API_KEY")
 
-    args = sys.argv[1:]
+    args = [arg for arg in sys.argv[1:] if not arg.startswith("--")]
 
     if not args:
         print("Tip:")
@@ -18,10 +21,10 @@ def main():
         print('Example: python3 main.py "How do I build a calculator app?"')
         sys.exit(1)
 
-    if "--verbose" not in args:
-        sys.exit(2)
-
     prompt = " ".join(args)
+
+    if verbose:
+        print(f"User prompt: {prompt}\n")
 
     messages = [
         types.Content(
@@ -33,6 +36,7 @@ def main():
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
+        config=types.GenerateContentConfig(system_instruction=system_prompt),
         )
 
     prompt_tokens = response.usage_metadata.prompt_token_count
